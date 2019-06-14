82bac04c1e103c3d8c885bc293aeaf21653ab300
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-13 19:16:19 -0700

    Add .net core projects

---
+++ SprocWrapperCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SprocWrapperCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
