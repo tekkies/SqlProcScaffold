3dcf7d426b2575cdaa310423bb5e3e5f60d48326
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-14 09:21:40 -0700

    Add indentation and write first 4 lines

--- SprocWrapperCore/ProcComposer.cs
+++ SprocWrapperCore/ProcComposer.cs
@@ -10,37 +10,78 @@ namespace SprocWrapper
         private readonly ProcIdentifier _procIdentifier;
         private StreamWriter _streamWriter;
         private string _namespace;
+        private int _indentationLevel=0;
+        private string _indentationPadding = String.Empty;
 
         public ProcComposer(SqlConnection sqlConnection, ProcIdentifier procIdentifier)
         {
             _sqlConnection = sqlConnection;
             _procIdentifier = procIdentifier;
-            _namespace = "Procs.dbo";
+            _namespace = "SprocWrapper.Procs";
         }
 
         public void Compose()
         {
-            _streamWriter = OpenStreamWriter();
-            using (_streamWriter)
+            using (_streamWriter = OpenStreamWriter())
             {
-                using (var dataReader = new Procs.Dbo.sp_procedure_params_rowset(_sqlConnection, _procIdentifier.Name, procedure_schema: _procIdentifier.Schema).ExecuteDataReader())
+                WriteUsings();
+                WriteNamespace();
+                OpenBrace();
+                GetParams();
+                CloseBrace();
+            }
+        }
+
+        private void CloseBrace()
+        {
+            SetIndentation(-1);
+            WriteLine("}");
+        }
+
+        private void GetParams()
+        {
+            using (var dataReader = new Procs.Dbo.sp_procedure_params_rowset(_sqlConnection, _procIdentifier.Name, procedure_schema: _procIdentifier.Schema).ExecuteDataReader())
+            {
+                while (dataReader.Read())
                 {
-                    while (dataReader.Read())
+                    for (int columnIndex = 0; columnIndex < dataReader.FieldCount; columnIndex++)
                     {
-                        for (int columnIndex = 0; columnIndex < dataReader.FieldCount; columnIndex++)
-                        {
-                            var line = String.Format("    {0}={1}", dataReader.GetName(columnIndex), dataReader[columnIndex]);
-                            Write(line);
-                        }
+                        var line = String.Format("{0}={1}", dataReader.GetName(columnIndex), dataReader[columnIndex]);
+                        WriteLine(line);
                     }
                 }
             }
         }
 
-        private void Write(string line)
+        private void OpenBrace()
         {
-            Logger.Log(line);
-            _streamWriter.WriteLine(line);
+            WriteLine("{");
+            SetIndentation(_indentationLevel + 1);
+        }
+
+        private void SetIndentation(int increment)
+        {
+            _indentationLevel += increment;
+            _indentationPadding = new String(' ', _indentationLevel*4);
+
+        }
+
+        private void WriteNamespace()
+        {
+            WriteLine($@"namespace {_namespace}");
+        }
+
+        private void WriteUsings()
+        {
+            WriteLine(@"using System.Data.SqlClient;");
+            WriteLine(String.Empty);
+        }
+
+        private void WriteLine(string text)
+        {
+            text = _indentationPadding + text;
+            //Logger.Log(text);
+            _streamWriter.WriteLine(text);
         }
 
         private StreamWriter OpenStreamWriter()
