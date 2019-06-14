3bc3ebbb7a12412d0caedfba27f95da784a89f5b
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-14 10:28:26 -0700

    More adventurous types

--- SprocWrapperCore/sp_sproc_wrapper_test.sql
+++ SprocWrapperCore/sp_sproc_wrapper_test.sql
@@ -5,6 +5,9 @@ GO
 CREATE PROCEDURE sp_sproc_wrapper_test
     @intNoDefault int,
     @intNullDefault int = null,
-    @intNumericDefault int = 1
+    @intNumericDefault int = 1,
+	@varcharNoDefault VARCHAR(20),
+	@varcharNullDefault VARCHAR(20) = NULL,
+	@varcharValueDefault VARCHAR(MAX) = 'max'
 AS
     SELECT @intNoDefault, @intNullDefault, @intNumericDefault
