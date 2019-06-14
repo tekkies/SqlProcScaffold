c91ded44f1a8cecc39a5b69b799d871d4b77def0
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-13 19:55:57 -0700

    Extract logic from program

--- SprocWrapper/Program.cs
+++ SprocWrapper/Program.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Data.SqlClient;
-
 namespace SprocWrapper
 {
     class Program
@@ -9,60 +6,7 @@ namespace SprocWrapper
         {
             var connectionString = args[0];
             var like = args[1];
-            Logger.Log(connectionString);
-            using (var sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                var procs = GetProcs(sqlConnection, like);
-                foreach (var proc in procs)
-                {
-                    GetParams(sqlConnection, proc);
-                }
-
-                new SprocWrapper.Procs.Dbo.sp_sproc_wrapper_test(sqlConnection, 1,2,3).Execute();
-
-            }
-
-        }
-
-        private static void GetParams(SqlConnection sqlConnection, string procName)
-        {
-            using (var dataReader = new SprocWrapper.Procs.Dbo.sp_procedure_params_rowset(sqlConnection, procName).ExecuteDataReader())
-            {
-                while (dataReader.Read())
-                {
-                    for (int columnIndex = 0; columnIndex < dataReader.FieldCount; columnIndex++)
-                    {
-                        Logger.Log("    {0}={1}", dataReader.GetName(columnIndex), dataReader[columnIndex]);
-                    }
-                }
-            }
-        }
-
-        private static List<string> GetProcs(SqlConnection sqlConnection, string like)
-        {
-            var procs = new List<string>();
-            using (var dataReader = new QueryBuilder(sqlConnection,
-                    @"SELECT  
-                    SS.name AS[schema],
-                    SO.name
-                        FROM sys.objects SO
-                    LEFT JOIN sys.schemas SS ON SO.schema_id = SS.schema_id
-                    WHERE SO.type = 'P'
-                    AND SO.name like @0",
-                    like)
-                .ExecuteDataReader())
-            {
-                while (dataReader.Read())
-                {
-                    var schema = dataReader.GetString(0);
-                    var procName = dataReader.GetString(1);
-                    procs.Add(procName);
-                    Logger.Log("[{0}].[{1}]", schema, procName);
-                }
-            }
-
-            return procs;
+            ProcWrapper.SprocWrapper(connectionString, like);
         }
     }
 }
