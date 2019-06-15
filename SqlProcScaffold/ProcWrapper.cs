1c093755e76a99aa7af14a65b1c2b72e87f69809
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-15 09:22:44 -0700

    Tidy around filter

--- SqlProcScaffold/ProcWrapper.cs
+++ SqlProcScaffold/ProcWrapper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
+using System.Net.Mime;
 using SqlProcScaffold.Properties;
 
 namespace SprocWrapper
@@ -18,11 +19,21 @@ namespace SprocWrapper
                 sqlConnection.Open();
                 WriteBaseClass();
                 var procs = GetProcs(sqlConnection, like);
+                CheckForNoProcs(procs);
                 foreach (var proc in procs)
                 {
                     var procComposer = new ProcComposer(sqlConnection, proc, _outputFolder);
                     procComposer.Compose();
                 }
+            }
+        }
+
+        private static void CheckForNoProcs(List<ProcIdentifier> procs)
+        {
+            if (procs.Count == 0)
+            {
+                Logger.Log("ERROR: No stored procedures found");
+                System.Environment.Exit(1);
             }
         }
 
@@ -46,7 +57,7 @@ namespace SprocWrapper
                         FROM sys.objects SO
                     LEFT JOIN sys.schemas SS ON SO.schema_id = SS.schema_id
                     WHERE SO.type = 'P'
-                    AND SO.name like @0",
+                    AND SS.name+'.'+SO.name like @0",
                     like)
                 .ExecuteDataReader())
             {
