c55b443dba870e41f4f11fc368c6432bd9d09804
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-14 17:15:26 -0700

    Set commandLineArgs

--- SprocWrapperCore/Program.cs
+++ SprocWrapperCore/Program.cs
@@ -8,7 +8,7 @@ namespace SprocWrapperCore
         static void Main(string[] args)
         {
             var connectionString = args[0];
-            var like = args[1];
+            var like = args.Length >= 2 ? args[1] : "%";
             ProcWrapper.SprocWrapper(connectionString, like);
         }
     }
