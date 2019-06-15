070d31b6a29f351ea3fb00d56b8a4d1efc1f652f
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-14 17:24:03 -0700

    Support DateTime

--- SprocWrapperCore/SqlToCSharpTypeMapper.cs
+++ SprocWrapperCore/SqlToCSharpTypeMapper.cs
@@ -8,13 +8,16 @@ namespace SprocWrapper
         public static string MapSqlToCSharp(string sqlType)
         {
             string cSharpType;
-            switch (sqlType)
+            switch (sqlType.ToLower())
             {
                 case "bit":
                     cSharpType = "bool";
                     break;
                 case "int":
                     cSharpType = "int";
+                    break;
+                case "datetime":
+                    cSharpType = "DateTime";
                     break;
                 case "char":
                 case "nchar":
@@ -25,7 +28,7 @@ namespace SprocWrapper
                     cSharpType = "string";
                     break;
                 default:
-                    throw new NotImplementedException($"Unable to map SQL data type {sqlType} to C# data type");
+                    throw new NotImplementedException($"Unable to map SQL data type \"{sqlType}\" to C# data type");
             }
             return cSharpType;
         }
