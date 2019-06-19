c2bb73f99b162b51f89d93ec3e123283cf91c671
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-18 20:49:14 -0700

    More consistent resource access
    Thanks to https://stackoverflow.com/a/41106851/270155

---
+++ SqlProcScaffold/Utils/ResourceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SqlProcScaffold.Utils
+{
+    internal class ResourceHelper
+    {
+        public static string GetResourceAsString(string name)
+        {
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream(name))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
