316822e2806e22a1d44e67bd6599093d23f4d245
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-18 22:12:53 -0700

    Moving forward

--- build.ps1
+++ build.ps1
@@ -1,1 +1,8 @@
 ﻿dotnet publish -c Release -f netcoreapp2.2 -r win10-x64 --self-contained false --output ..\choco\SqlProcScaffold\tools SqlProcScaffold\SqlProcScaffold.csproj
+Push-Location
+try {
+    Set-Location .\choco\SqlProcScaffold
+    choco pack
+} finally {
+    Pop-Location
+}
