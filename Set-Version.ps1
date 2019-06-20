2b5ea09a2fc144de48519de2a0134b3eee4fea04
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-06-19 22:25:39 -0700

    Set version

---
+++ Set-Version.ps1
@@ -0,0 +1,8 @@
+﻿param ( $newVersion )
+
+((Get-Content .\SqlProcScaffold\SqlProcScaffold.csproj) `
+    -replace '<Version>\d+.\d+.\d+</Version>', "<Version>$newVersion</Version>") `
+    | Out-File .\SqlProcScaffold\SqlProcScaffold.csproj
+((Get-Content .\choco\SqlProcScaffold\sqlprocscaffold.nuspec) `
+    -replace '<Version>\d+.\d+.\d+</Version>', "<Version>$newVersion</Version>") `
+    | Out-File .\choco\SqlProcScaffold\sqlprocscaffold.nuspec
